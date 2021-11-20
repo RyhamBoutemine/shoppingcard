cca3398efde3367c0df21434a7a6908c755cc8d2
RyhamBoutemine <ryhamboutemine@gmail.com>
2021-11-20 15:53:22 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 var qtPlus = document.querySelectorAll(".svg-icon3");
 var qtMoins = document.querySelectorAll(".svg-icon4");
+var prixTotal = document.querySelectorAll(".prixTotal")
 var qt = document.querySelectorAll(".qt");
 var prix= document.querySelectorAll(".prix");
-console.log(qtPlus);
-console.log(qtMoins);
-console.log(qt);
+var prixP = document.querySelector(".prixPanier");
+// console.log(qtPlus);
+// console.log(qtMoins);
+// console.log(qt);
+// console.log(prixP);
 for(let i=0; i<qtPlus.length;i++){
     qtPlus[i].addEventListener("click", function(){ 
         let q = parseInt(qt[i].innerHTML); 
         qt[i].innerHTML=q+1;
-
+        let p = parseInt(prixTotal[i].innerHTML);
+        prixTotal[i].innerHTML= p+parseFloat(prix[i].innerHTML) + "$";
+        prixP.innerHTML = parseFloat(prixP.innerHTML)+ parseFloat(prix[i].innerHTML) +" $";
+        console.log(parseFloat(prixP.innerHTML));
      })
 }
 
@@ -19,6 +25,10 @@ for(let i=0; i<qtMoins.length;i++){
         if(q >0){
             qt[i].innerHTML=q-1;
         }
-        
+        qt[i].innerHTML=q-1;
+        let p = parseInt(prixTotal[i].innerHTML);
+        prixTotal[i].innerHTML= p-parseFloat(prix[i].innerHTML) + "$";
+        prixP.innerHTML = parseFloat(prixP.innerHTML)- parseFloat(prix[i].innerHTML) +" $"
+        console.log(prixP.innerHTML);
      })
 }
